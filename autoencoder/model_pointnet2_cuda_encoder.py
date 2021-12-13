263d66920c3f3686c946bef13fcf30fe9b9332e6
nkleber <nils.kleber@freenet.de>
2021-12-13 12:02:00 +0100

    debugging (11)

--- autoencoder/model_pointnet2_cuda_encoder.py
+++ autoencoder/model_pointnet2_cuda_encoder.py
@@ -4,111 +4,12 @@ This code is based on the implementation of Erik Wijmans
 https://github.com/erikwijmans/Pointnet2_PyTorch
 """
 
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from pointnet2_ops import pointnet2_utils
 from pointnet2_ops.pointnet2_modules import PointnetSAModule
 from typing import List, Optional, Tuple
 import pytorch_lightning as pl
 
 
-# def build_shared_mlp(mlp_spec: List[int], bn: bool = True):
-#     layers = []
-#     for i in range(1, len(mlp_spec)):
-#         layers.append(
-#             nn.Conv2d(mlp_spec[i - 1], mlp_spec[i], kernel_size=1, bias=not bn)
-#         )
-#         if bn:
-#             layers.append(nn.BatchNorm2d(mlp_spec[i]))
-#         layers.append(nn.ReLU(True))
-#
-#     return nn.Sequential(*layers)
-#
-#
-# class PointnetSAModule(nn.Module):
-#     r"""Pointnet set abstrction layer
-#     Parameters
-#     ----------
-#     n_points : int
-#         Number of features
-#     radius : float
-#         Radius of ball
-#     n_sample : int
-#         Number of samples in the ball query
-#     mlp : list
-#         Spec of the pointnet before the global max_pool
-#     bn : bool
-#         Use batchnorm
-#     """
-#
-#     def __init__(
-#         self, mlp, n_points=None, radius=None, n_sample=None, bn=True, use_xyz=True):
-#         # type: (PointnetSAModule, List[int], int, float, int, bool, bool) -> None
-#         super(PointnetSAModule, self).__init__()
-#
-#         self.n_points = n_points
-#         self.groupers = nn.ModuleList()
-#         self.mlps = nn.ModuleList()
-#
-#         self.groupers.append(
-#             pointnet2_utils.QueryAndGroup(radius, n_sample, use_xyz=use_xyz)
-#             if n_points is not None
-#             else pointnet2_utils.GroupAll(use_xyz)
-#         )
-#         if use_xyz:
-#             mlp[0] += 2
-#         self.mlps.append(build_shared_mlp(mlp, bn))
-#
-#     def forward(self, xyz, points):
-#         r"""
-#         Parameters
-#         ----------
-#         xyz : torch.Tensor
-#             (B, N, 3) tensor of the xyz coordinates of the features
-#         features : torch.Tensor
-#             (B, C, N) tensor of the descriptors of the the features
-#         Returns
-#         -------
-#         new_xyz : torch.Tensor
-#             (B, npoint, 3) tensor of the new features' xyz
-#         new_features : torch.Tensor
-#             (B,  \sum_k(mlps[k][-1]), npoint) tensor of the new_features descriptors
-#         """
-#         new_features_list = []
-#
-#         xyz_flipped = xyz.transpose(1, 2).contiguous()
-#         new_xyz = (
-#             pointnet2_utils.gather_operation(
-#                 xyz_flipped, pointnet2_utils.furthest_point_sample(xyz, self.n_points)
-#             )
-#             .transpose(1, 2)
-#             .contiguous()
-#             if self.n_points is not None
-#             else None
-#         )
-#
-#         for i in range(len(self.groupers)):
-#             print(i)
-#             print(points)
-#             new_points = self.groupers[i](
-#                 xyz, new_xyz, points
-#             )  # (B, C, npoint, nsample)
-#             # weight of size 64 4 1 1,
-#             # expected input[64, 2, 512, 64]
-#             # to have 4 channels, but got 2 channels instead
-#             print(new_points.shape)
-#             new_points = self.mlps[i](new_points)  # (B, mlp[-1], npoint, nsample)
-#             new_points = F.max_pool2d(
-#                 new_points, kernel_size=[1, new_points.size(3)]
-#             )  # (B, mlp[-1], npoint, 1)
-#             new_points = new_points.squeeze(-1)  # (B, mlp[-1], npoint)
-#
-#             new_points.append(new_points)
-#
-#         return new_xyz, torch.cat(new_features_list, dim=1)
-#
-#
 def _break_up_pc(pc):
     xyz = pc[..., 0:3].contiguous()
     features = pc[..., 3:].transpose(1, 2).contiguous() if pc.size(-1) > 3 else None
