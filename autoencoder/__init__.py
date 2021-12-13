2788a0a349c6fbead19a455500dbe9e272f841e6
nkleber <nils.kleber@freenet.de>
2021-12-13 23:51:10 +0100

    add from .model_pointnet2_cuda_encoder import PointNet2CudaEncoder (again)

--- autoencoder/__init__.py
+++ autoencoder/__init__.py
@@ -1,6 +1,6 @@
 from .model_graph_encoder import GraphEncoder
 from .model_pointnet2_encoder import PointNet2Encoder
-# from .model_pointnet2_cuda_encoder import PointNet2CudaEncoder
+from .model_pointnet2_cuda_encoder import PointNet2CudaEncoder
 from .model_pointnet_encoder import PointNetEncoder
 from .model_dense_encoder import DenseEncoder
 from .model_fold_decoder import FoldDecoder
