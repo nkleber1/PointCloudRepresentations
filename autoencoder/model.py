085cc4725851b444d06d71c22adf91e8705f2b34
nkleber <nils.kleber@freenet.de>
2022-01-07 11:01:45 +0100

    add vae bottleneck

--- autoencoder/model.py
+++ autoencoder/model.py
@@ -1,5 +1,6 @@
+import torch
 import torch.nn as nn
-from . import GraphEncoder, PointNet2Encoder, PointNetEncoder, DenseEncoder, FoldDecoder, DenseDecoder, PointNet2CudaEncoder
+from . import VAEBottleneck, GraphEncoder, PointNet2Encoder, PointNetEncoder, DenseEncoder, FoldDecoder, DenseDecoder  # ,PointNet2CudaEncoder
 from .loss import ChamferLoss
 
 
@@ -23,9 +24,13 @@ class ReconstructionNet(nn.Module):
         elif args.decoder == 'dense':
             self.decoder = DenseDecoder(args)
         self.loss = ChamferLoss()
+        self.args = args
+        self.vae_bottleneck = VAEBottleneck(args)
 
     def forward(self, input):
         feature = self.encoder(input)
+        if self.args.vae:
+            feature = self.vae_bottleneck(feature)
         output = self.decoder(feature)
         return output, feature
 
@@ -34,3 +39,16 @@ class ReconstructionNet(nn.Module):
 
     def get_loss(self, input, output):
         return self.loss(input, output)
+
+    def load_pretrain(self, pretrain):
+        state_dict = torch.load(pretrain, map_location='cpu')
+        from collections import OrderedDict
+        new_state_dict = OrderedDict()
+        for key, val in state_dict.items():
+            if key[:6] == 'module':
+                name = key[7:]  # remove 'module.'
+            else:
+                name = key
+            new_state_dict[name] = val
+        self.load_state_dict(new_state_dict)
+        print(f"Load model from {pretrain}")
---
+++ autoencoder/model_vae_bottleneck.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+
+class VAEBottleneck(nn.Module):
+    def __init__(self, args):
+        super(VAEBottleneck, self).__init__()
+        self.mu = nn.Linear(args.feat_dims, args.feat_dims)
+        self.var = nn.Linear(args.feat_dims, args.feat_dims)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        # Split the result into mu and var components
+        mu = self.mu(x)
+        log_var = self.var(x)
+        # compute std
+        std = torch.exp(0.5 * log_var)
+        # sample eps
+        eps = torch.randn_like(std)
+        # “Reparameterization” trick
+        z = mu + eps * std
+        z = torch.unsqueeze(z, dim=1)
+        return z
