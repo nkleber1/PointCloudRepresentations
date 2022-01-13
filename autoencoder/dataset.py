7f4ccd6177db48432acbdfcde4cef17d94a46360
nkleber <nils.kleber@freenet.de>
2022-01-13 18:22:13 +0100

    make original datasets available

--- autoencoder/dataset.py
+++ autoencoder/dataset.py
@@ -13,6 +13,12 @@ class PointCloudDataset:
     def __init__(self, args):
         if args.dataset == 'lidar':
             file = 'data/train_data/lidar_512.npy'
+        elif args.dataset == 'original_uni':
+            file = 'data/train_data/original_uni.npy'
+        elif args.dataset == 'original_lidar':
+            file = 'data/train_data/original_lidar.npy'
+        elif args.dataset == 'uniform':
+            file = 'data/train_data/uniform.npy'
         elif args.dataset == 'curriculum_uniform':
             file = 'data/train_data/curriculum_uniform/level{}_train.npy'.format(args.level)
         elif args.dataset == 'curriculum_lidar':
@@ -53,6 +59,10 @@ class PointCloudEvalDataset:
     def __init__(self, args):
         if args.dataset == 'lidar':
             file = 'data/eval_data/lidar_512_eval.npy'
+        elif args.dataset == 'original_uni':
+            file = 'data/eval_data/original_uni.npy'
+        elif args.dataset == 'original_lidar':
+            file = 'data/eval_data/original_lidar.npy'
         elif args.dataset == 'curriculum_uniform':
             file = 'data/eval_data/curriculum_uniform/level{}_eval.npy'.format(args.level)
         elif args.dataset == 'curriculum_lidar':
